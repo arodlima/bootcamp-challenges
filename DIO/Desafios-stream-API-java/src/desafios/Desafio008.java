70dedf6dcf80db01294a9044e157ca57064be001
Andreza Lima <andrezalima.co@gmail.com>
2023-10-04 13:44:04 -0300

    stream api challenge

--- DIO/Desafios-stream-API-java/src/desafios/Desafio008.java
+++ DIO/Desafios-stream-API-java/src/desafios/Desafio008.java
@@ -2,6 +2,7 @@ package desafios;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.IntStream;
 
 public class Desafio008 {
     public static void main(String[] args) {
@@ -9,9 +10,17 @@ public class Desafio008 {
 
         List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
 
-        int soma = numeros.stream()
-                .reduce(0, Integer::sum);
+        int somaDosDigitos = numeros.stream()
+                .flatMapToInt(Desafio008::separarDigitos)
+                .distinct()
+                .sum();
 
-        System.out.println("A soma dos números é: " + soma);
+        System.out.println("A soma dos digítos é: " + somaDosDigitos);
+    }
+
+    private static IntStream separarDigitos(int numero) {
+        return String.valueOf(numero)
+                .chars()
+                .map(Character::getNumericValue);
     }
 }
